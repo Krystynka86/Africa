942733cd251c6779b416397b04698543e452a61d
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-13 16:38:23 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Cristina on 2023-07-13.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

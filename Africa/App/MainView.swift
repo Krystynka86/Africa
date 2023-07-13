2b3d15aafb57e6a611b79d1b54ee4c023ce94564
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-13 17:29:57 +0200

    add view files to App

---
+++ Africa/App/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  Africa
+//
+//  Created by Cristina on 2023-07-13.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView() {
+            
+        } //: TAB
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Cristina on 2023-07-13.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}

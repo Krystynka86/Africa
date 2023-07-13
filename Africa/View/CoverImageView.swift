1bab5b22dd6aef14c50775828af3ff9ee8219255
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-13 18:10:59 +0200

    add cover image

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,32 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Cristina on 2023-07-13.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                    .scaledToFill()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+// MARK: - PREVIEW
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}

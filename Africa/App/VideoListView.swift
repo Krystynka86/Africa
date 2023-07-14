8cdbaf00863156fc4d353e3c37deee2b85156981
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-14 20:04:29 +0200

    add videoListItem to VideoListView + refactoring

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button( action: {
+                        videos.shuffle()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        } //: NAVIGATION
     }
 }
 
+// MARK: - PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()

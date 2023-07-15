3e7a711c83cc34a1c0f4760265b46a087ed2255a
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-15 21:34:59 +0200

    add basic map to MapView

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -11,9 +11,18 @@ import MapKit
 struct MapView: View {
     // MARK: - PROPERTIES
     
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
     // MARK: - BODY
     var body: some View {
-        Text("Map")
+        // MARK: - No1 BASIC MAP
+        Map(coordinateRegion: $region)
     }
 }
 

507f0a21b85e60bbcb3b5d8b5f5962015d706cc0
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-17 13:16:45 +0200

    add advance grid system switch to content view

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -16,8 +16,29 @@ struct ContentView: View {
     
     @State private var isGridViewActive: Bool = false
     
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
     
+    // MARK: FUNCTIONS
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid Number: \(gridColumn)")
+    
+        // TOOLBAR IMAGE
+        switch gridColumn {
+        case 1:
+          toolbarIcon = "square.grid.2x2"
+        case 2:
+          toolbarIcon = "square.grid.3x2"
+        case 3:
+          toolbarIcon = "rectangle.grid.1x2"
+        default:
+          toolbarIcon = "square.grid.2x2"
+        }
+      }
+
     var body: some View {
         
         // MARK: - BODY
@@ -56,7 +77,7 @@ struct ContentView: View {
                     HStack(spacing: 12) {
                         // LIST
                         Button(action: {
-                            print("list view is avtivated")
+                            print("List view is avtivated")
                             isGridViewActive = false
                             haptics.impactOccurred()
                         }) {
@@ -70,8 +91,9 @@ struct ContentView: View {
                             print("Grid view is avtivated")
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            gridSwitch()
                         }) {
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
                         }

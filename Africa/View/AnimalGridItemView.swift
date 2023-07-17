72d6faa9f03e7b9aa1ff45b85c5460fcfe6cde5a
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-17 11:15:26 +0200

    create AnimalGridItem

---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,32 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Cristina on 2023-07-17.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    // MARK: PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: BODY
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFill()
+            .cornerRadius(12)
+    }
+}
+
+// MARK: PREVIEW
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
